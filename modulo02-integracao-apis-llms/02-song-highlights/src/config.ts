77416bdaa92aced2b7abe181a74ce4cb5fecc088
Well <wellytonsdj@gmail.com>
2026-06-22 22:49:32 -0300

    adding other models for test

--- modulo02-integracao-apis-llms/02-song-highlights/src/config.ts
+++ modulo02-integracao-apis-llms/02-song-highlights/src/config.ts
@@ -17,27 +17,34 @@ export type ModelConfig = {
     dbUri: string;
   };
   maxMessagesToSummary: number;
-
 };
 
-console.assert(process.env.OPENROUTER_API_KEY, 'OPENROUTER_API_KEY is not set in environment variables');
+console.assert(
+  process.env.OPENROUTER_API_KEY,
+  "OPENROUTER_API_KEY is not set in environment variables",
+);
 
 export const config: ModelConfig = {
   apiKey: process.env.OPENROUTER_API_KEY!,
-  httpReferer: '',
-  xTitle: 'IA Devs - Prompt Chaining Article Generator',
+  httpReferer: "",
+  xTitle: "IA Devs - Prompt Chaining Article Generator",
   models: [
-    'arcee-ai/trinity-large-preview:free',
+    // "arcee-ai/trinity-large-preview:free",
+    // top 4 para a listagem ordenada por preço
+    // teste
+    // "nvidia/nemotron-3-ultra-550b-a55b:free",
+    "poolside/laguna-xs.2:free",
   ],
   provider: {
     sort: {
-      by: 'throughput', // Route to model with highest throughput (fastest response)
-      partition: 'none',
+      by: "throughput", // Route to model with highest throughput (fastest response)
+      partition: "none",
     },
   },
   temperature: 0.7,
   memory: {
-    dbUri: 'postgresql://postgres:mysecretpassword@localhost:5432/song_recommender',
+    dbUri:
+      "postgresql://postgres:mysecretpassword@localhost:5432/song_recommender",
   },
-  maxMessagesToSummary: 2
+  maxMessagesToSummary: 2,
 };
